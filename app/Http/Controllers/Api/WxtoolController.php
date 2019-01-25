4e0ac42735fe4f76eff2b357abdc788c8665ebeb
kaka <1169656090@qq.com>
2019-01-25 13:39:37 +0800

    modify wx

--- app/Http/Controllers/Api/WxtoolController.php
+++ app/Http/Controllers/Api/WxtoolController.php
@@ -15,17 +15,17 @@ class WxtoolController extends Controller
         $timestamp = $request->input('timestamp');
         $echostr = $request->input('echostr');
         $nonce = $request->input('nonce');
-        $token = 'msqlx0801';
+        $token = config('wx.Token');
         $array = array($token,$nonce,$timestamp);
         sort($array);
         $hashcode = sha1(implode('', $array));
         if($hashcode == $signature && $echostr){
-            echo $echostr;//微信回调验证
-            exit;
+            return $echostr;//微信回调验证
         }else{
-//            $this->assWeChat();//用户事件
+            return false;
         }
     }
+
     /**
      * 关注服务号事件
      */
@@ -37,8 +37,7 @@ class WxtoolController extends Controller
         $EventKey = $obj->EventKey; //获取场景ID
         if(strtolower($obj->MsgType)=='event'){//事件
             if(strtolower($obj->Event)=='subscribe' || strtolower($obj->Event)=='scan'){//关注事件
-                $shop_code = trim($EventKey,'qrscene_B');
-                $unionId = $this->getUserUnionId($FromUserName);//获取用户  unionId
+//                $unionId = $this->getUserUnionId($FromUserName);//获取用户  unionId
                 $content = '欢迎来到雪球社区！！！';
                 $temp =  $this->getXML($FromUserName,$ToUserName,$content);//XML回复微信服务号
                 echo $temp;
@@ -62,7 +61,18 @@ class WxtoolController extends Controller
     }
 
     /**
-     * 设置公众号底部导航o
+     * 获取 wxcool_access_token
+     * 返回json格式   $access_token 512字符   expires_in：有效期  7200   默认两小时
+     */
+    public function getWxAccessToken(){
+        $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".config('wx.AppID')."&secret=".config('wx.AppSecret');
+        $obj = file_get_contents($url);
+        $obj = json_decode($obj,true);
+        return $obj['access_token'];
+    }
+
+    /**
+     * 设置公众号底部导航
      */
     public function setFooterButton(){
         $access_token = $this->getWxAccessToken();
@@ -71,34 +81,84 @@ class WxtoolController extends Controller
             "button": [
                 {
                     "type": "miniprogram",
-                    "name": "附近好店小程序",
-                    "url": "https://www.menkouhaodian.com",
-                    "appid": "wxae25568b6c321390",
+                    "name": "雪球社区",
+                    "url": "https://www.msqlx.com",
+                    "appid": "wx943a69cda83ce6f6",
                     "pagepath": "pages/index/index"
                 },
                 {
                     "type": "click",
-                    "name": "小助手",
+                    "name": "热门服务",
                     "sub_button": [
-                        {
-                           "type":"click",
-                           "name":"排行榜",
-                           "key":"V1001_RANKINGLIST"
-                        },
                          {
                            "type":"view",
-                           "name":"APP下载",
-                           "url":"http://a.app.qq.com/o/simple.jsp?pkgname=com.renwumeng.haodian"
-                        },
-                        {
-                           "type":"click",
-                           "name":"我的二维码",
-                           "key":"V1001_MYCODE"
-                        },
-                   ]
+                           "name":"社区入驻",
+                           "url":""
+                         }，
+                         {
+                           "type":"view",
+                           "name":"商家入驻",
+                           "url":""
+                         }，
+                         {
+                           "type":"view",
+                           "name":"旅游入驻",
+                           "url":""
+                         }
+                    ]
+                }
+                {
+                    "type": "click",
+                    "name": "关于我们",
+                    "sub_button": [
+                         {
+                           "type":"view",
+                           "name":"问题反馈",
+                           "url":""
+                         }，
+                         {
+                           "type":"view",
+                           "name":"帮助中心",
+                           "url":""
+                         }，
+                         {
+                           "type":"view",
+                           "name":"联系我们",
+                           "url":""
+                         }，
+                         {
+                           "type":"view",
+                           "name":"广告合作",
+                           "url":""
+                         }
+                    ]
                 }
             ]
         }';
         $this->http_post_curl($url,$data);
+    }
+
+    /**
+     * curl post 请求
+     * 参数 $url：微信公众号各接口地址  data 数据参数
+     * 返回 obj
+     */
+    private function http_post_curl($url,$data){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        if($output !== FALSE ){
+            $obj = json_decode($output);
+            return $obj;
+        }else{
+            return false;
+        }
     }
 }
---
+++ config/wx.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'Token' => 'msqlx0801',
+    'AppID' => 'wx3ee46e3c134497a6',
+    'AppSecret' => '2ea301b8136201986aea4fbbaaf1011f'
+];
