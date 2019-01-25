d96a9b6b6fe228256a34122ee1e8c852cc2c5718
kaka <1169656090@qq.com>
2019-01-25 13:42:17 +0800

    add apiroute

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
  * 公众号接口
  */
 Route::get('wx_index','Api\WxtoolController@index');//公众号入口
+Route::get('setFooterButton','Api\WxtoolController@setFooterButton');//设置底部导航栏
