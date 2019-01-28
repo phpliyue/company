10612b87eff96436f154b90788841ab46c67e2e2
kaka <1169656090@qq.com>
2019-01-28 11:13:49 +0800

    add

--- app/Http/Controllers/Api/WxtoolController.php
+++ app/Http/Controllers/Api/WxtoolController.php
@@ -71,9 +71,10 @@ class WxtoolController extends Controller
             DB::table('meisi')->insert(['title'=> $mes]);
 //            $temp = $this->getXML($postObj->FromUserName,$postObj->ToUserName,$mes);
 //            $temp = $this->getXML($fromUser,$toUser,$mes);//XML回复微信服务号
-            $tpl = "<xml><Content><![CDATA[aaaaaaa]]></Content></xml>";
-            DB::table('meisi')->insert(['title'=>$tpl]);
-            echo $tpl;
+            $tpl = "<xml><Content><![CDATA[%s]]></Content></xml>";
+            $temp = sprintf($tpl,$mes);
+            DB::table('meisi')->insert(['title'=>$temp]);
+            echo $temp;
         }
     }
 
