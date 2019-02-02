0c40bca44be4bbe8b0d3361234e8519af17c4e3e
phpliyue <qmna.20@gmail.com>
2019-02-03 01:55:01 +0800

    0

--- app/Http/Controllers/Api/WxtoolController.php
+++ app/Http/Controllers/Api/WxtoolController.php
@@ -62,17 +62,16 @@ class WxtoolController extends Controller
                     $content = '欢迎关注我们的微信公众账号';
                     break;
             }
-            $time = time();
             //回复用户消息(纯文本格式)
             DB::table('meisi')->insert(['title'=>$key.','.$fromUser.','.$toUser]);
             $temp = $this->getXML($toUser,$fromUser,$content);
             echo "<xml>
-     <ToUserName>$toUser</ToUserName>
-     <FromUserName>$fromUser</FromUserName>
-     <CreateTime>$time</CreateTime>
-     <MsgType>text</MsgType>
-     <Content>$content</Content>
-     </xml>";
+ <ToUserName>$toUser</ToUserName>
+ <FromUserName>$fromUser</FromUserName>
+ <CreateTime>1395658920</CreateTime>
+ <MsgType>text</MsgType>
+ <Content>$content</Content>
+ </xml>";
         }
         if(strtolower($postObj->MsgType) == 'text'){
             return view('weixin.index',['message'=>$postObj]);
@@ -90,14 +89,13 @@ class WxtoolController extends Controller
      */
     public function getXML($toUser,$fromUser,$content)
     {
-      $time = time();
         $tpl = "<xml>
- <ToUserName>$toUser</ToUserName>
- <FromUserName>$fromUser</FromUserName>
- <CreateTime>$time</CreateTime>
- <MsgType>text</MsgType>
- <Content>$content</Content>
- </xml>";
+           <ToUserName><![CDATA[%s]]></ToUserName>
+           <FromUserName><![CDATA[%s]]></FromUserName>
+           <CreateTime>%d</CreateTime>
+           <MsgType><![CDATA[text]]></MsgType>
+           <Content><![CDATA[%s]]></Content>
+           </xml>";
 
         DB::table('meisi')->insert(['title'=> $tpl]);
        $temp = sprintf($tpl,$toUser,$fromUser,time(),$content);
